722567e89cc02e4119fb8f66e1f76f8da5fc5241
jawsman0 <evincze@live.com>
2015-11-22 15:18:08 -0500

    Updated Project RPG

--- RPG/RPG.cs
+++ RPG/RPG.cs
@@ -5,7 +5,10 @@ class RPG
 {
 	public static string playerName;
 	public static string playerConfirmation;
+	public static string playerGender;
 	public static string friendsName;
+	public static string friendsConfirmation;
+	public static string friendsGender;
 
 	public static void Main ()
 	{
@@ -62,6 +65,42 @@ class RPG
 
 	public static void FriendName ()
 	{
+		friendsName = Console.ReadLine ();
+		Console.WriteLine ("");
+		Console.WriteLine ("Do You Confirm That Your Friend's Name Is " + friendsName + "? y/n");
+		FriendsName2 ();
+	}
 
+	public static void FriendsName2 ()
+	{
+		friendsConfirmation = Console.ReadLine ();
+		Console.WriteLine ("");
+		if (friendsConfirmation == "y")
+		{
+			Console.WriteLine ("Sweet We Can Almost Start The Game");
+			Continuing ();
+		}else if (friendsConfirmation == "n")
+		{
+			Console.WriteLine ("That's Okay Just Hurry The Enemies Are Coming Rewrite Your Friends Name Please!!");
+			FriendName ();
+		}else 
+		{
+			Console.WriteLine ("That Isn't A Answer Try Again");
+			Console.WriteLine ("Do You Confirm That Your Friends Name Is " + friendsName + "? y/n");
+			FriendsName2 ();
+		}
+
+	}
+
+	public static void Continuing ()
+	{
+		Console.WriteLine ("");
+		Console.ForegroundColor = ConsoleColor.Cyan;
+		Console.WriteLine ("Are You A Boy Or A Girl");
+		playerGender = Console.ReadLine ();
+		if (playerGender == "Boy")
+		{
+
+		}
 	}
 }
